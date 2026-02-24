5cac42028aa106dd6d138536e67eae04796f7801
Fida-Ukwishaka <fidaukwishaka@gmail.com>
2026-02-24 06:32:39 +0000

    Instructor classs extends Person

---
+++ Lab1/Instructor.java
@@ -0,0 +1,29 @@
+public class Instructor extends Person {
+
+    private String department;
+
+
+    Instructor(String name, int age,  String department) {
+        super(name, age);
+        this.department = department;
+    }
+
+
+    public String getDepartment() {
+        return department;
+    }
+
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+
+    @Override
+    public String getAcademicLevel(){
+        return "Instructor";
+    }
+
+    
+    
+}
