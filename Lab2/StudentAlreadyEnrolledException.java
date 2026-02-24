9ec999f929caf700edeb7a3c9b25184cad4e76c6
Fida-Ukwishaka <fidaukwishaka@gmail.com>
2026-02-24 12:53:55 +0000

    Enrolling once in a course

---
+++ Lab2/StudentAlreadyEnrolledException.java
@@ -0,0 +1,9 @@
+package lab2.exceptions;
+
+public class StudentAlreadyEnrolledException extends Exception{
+
+    public StudentAlreadyEnrolledException(String message){
+
+        super(message);
+    }
+}
